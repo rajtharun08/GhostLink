e447026d038653cfc572ecc8fe7d01c660125947
Tharun R <rajtharun08@gmail.com>
2026-02-16 19:26:51 +0530

    feat: add Pydantic models for request validation

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 from app.database import init_db
+from pydantic import BaseModel, HttpUrl
 
 #initialize db
 init_db()
 
 app=FastAPI(title="GhostLink")
+
+class LinkCreate(BaseModel):
+    long_url: HttpUrl
+    max_clicks: int = 1
+    ttl_hours: int = 24
 
 @app.get("/")
 def health():
