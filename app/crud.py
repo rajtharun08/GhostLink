6f693df72af22426fccfebaf43aad7d55dfb1b4e
Tharun R <rajtharun08@gmail.com>
2026-02-16 19:34:30 +0530

    feat: add deletion logic for ghosting links

--- app/crud.py
+++ app/crud.py
@@ -19,3 +19,18 @@ def get_link(conn, short_code):
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM links WHERE short_code = ?", (short_code,))
     return cursor.fetchone()
+
+def increment_clicks(conn, short_code):
+    """Adds 1 to the current click count."""
+    cursor = conn.cursor()
+    cursor.execute(
+        "UPDATE links SET current_clicks = current_clicks + 1 WHERE short_code = ?",
+        (short_code,)
+    )
+    conn.commit()
+
+def delete_link(conn, short_code):
+    """Permanently removes a link from the database."""
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM links WHERE short_code = ?", (short_code,))
+    conn.commit()
