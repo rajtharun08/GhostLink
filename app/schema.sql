93a5ac3f65f3f462fc36e25e219e67d55bef7679
Tharun R <rajtharun08@gmail.com>
2026-02-16 18:47:57 +0530

    feat: define database schema for links table

---
+++ app/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS links (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    short_code TEXT UNIQUE NOT NULL,
+    long_url TEXT NOT NULL,
+    max_clicks INTEGER NOT NULL,
+    current_clicks INTEGER DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    expires_at TIMESTAMP NOT NULL
+);
